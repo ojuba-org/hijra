71bc4a08bfa83f71406d0d8d6d64ded5e0b95ad3
Ehab El-Gedawy <ehabsas@gmail.com>
2011-12-31 03:45:34 +0200

    fix gnome-3.2.x compatibility issue

--- gnome-shell-extension/extension.js
+++ gnome-shell-extension/extension.js
@@ -1,6 +1,26 @@
+// FIXME: Toggle comment lines (3-$), To add enable and disable functions 
+// ^^ But we must enable HijtaApplet extension after install it.
+/*
+//const Panel = imports.ui.panel;
+const StatusIconDispatcher = imports.ui.statusIconDispatcher;
+
+function init(){
+}
+
+function enable() {
+  StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['hijriapplet'] = 'hijriapplet';
+}
+
+function disable() {
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['hijriapplet'] = '';
+}
+*/
+
 const Panel = imports.ui.panel;
 const StatusIconDispatcher = imports.ui.statusIconDispatcher;
 
 function init() {
     StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['hijriapplet'] = 'hijriapplet';
 }
+
+

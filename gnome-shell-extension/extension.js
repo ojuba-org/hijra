cb628d7392e0f6fafd4cb76ec8c490d18350cbf8
Ehab El-Gedawy <ehabsas@gmail.com>
2012-01-02 01:18:57 +0200

    add extension enable/disable functions

--- gnome-shell-extension/extension.js
+++ gnome-shell-extension/extension.js
@@ -1,6 +1,4 @@
-// FIXME: Toggle comment lines (3-$), To add enable and disable functions 
-// ^^ But we must enable HijtaApplet extension after install it.
-/*
+
 //const Panel = imports.ui.panel;
 const StatusIconDispatcher = imports.ui.statusIconDispatcher;
 
@@ -14,13 +12,4 @@ function enable() {
 function disable() {
     StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['hijriapplet'] = '';
 }
-*/
-
-const Panel = imports.ui.panel;
-const StatusIconDispatcher = imports.ui.statusIconDispatcher;
-
-function init() {
-    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['hijriapplet'] = 'hijriapplet';
-}
-
 
